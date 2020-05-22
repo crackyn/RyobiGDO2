116151080237e2d4d251f99b035056702dd1a140
Fletcher, Chip <Chip.Fletcher@ncr.com>
2020-05-22 12:52:13 -0400

    First commit

---
+++ DebugGDOProxy.js
@@ -0,0 +1,177 @@
+// Project Name: Ryobi GDO Proxy for Node.js
+// Version: 2.0
+// Author: Justin Dybedahl
+//
+// https://github.com/Madj42/RyobiGDO/
+//
+
+
+const http = require('http')
+const url = require('url')
+var WebSocket = require('ws')
+const port = 3042
+
+const requestHandler = (request, response) => {
+const queryData = url.parse(request.url, true).query;
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    var reqip = request.connection.remoteAddress.split(':')
+    //if (reqip[3] !== 'x.x.x.x') {
+    //    response.end('Not Authorized')
+    //}
+    //console.log(request.url)
+    if (queryData.name == 'lighton') {
+        var cmd = 'lightState'
+        var cmdstate = 'true'
+        var cmdtype = 0
+    } else if (queryData.name == 'lightoff') {
+        var cmd = 'lightState'
+        var cmdstate = 'false'
+        var cmdtype = 0
+    } else if (queryData.name == 'dooropen') {
+        var cmd = 'doorCommand'
+        var cmdstate = '1'
+        var cmdtype = 0
+    } else if (queryData.name == 'doorclose') {
+        var cmd = 'doorCommand'
+        var cmdstate = '0'
+        var cmdtype = 0
+    } else if (queryData.name == 'status') {
+        var cmd = 'status'
+        var cmdtype = 1
+    } else {
+        response.end('Not valid command!');
+    }
+
+    if (queryData.name == null) {
+        response.end('No name specified');
+    } else if (queryData.email == null) {
+        response.end('No email specified');
+    } else if (queryData.pass == null) {
+        response.end('No password specified');
+    } else if (queryData.doorid == null) {
+        response.end('No door id specified');
+    }
+    var doorid = queryData.doorid;
+	var cleanpass = queryData.pass.replace(/[<>+\/'"*()?]/g, "\\$&");
+    var request = require('request');
+    const getAPIKey = () => new Promise((resolve, reject) => {
+//        console.log(queryData.email)
+//        console.log(queryData.pass)
+        var options = {url:'https://tti.tiwiconnect.com/api/login',method:'POST',json:JSON.parse('{"username":"' + queryData.email + '","password":"' + cleanpass + '"}')}
+        request(options, (err, res, body) => {
+            if (err) return reject(err)
+                resolve(body)
+            })
+        })
+
+        const getAPIKeyController = async function() {
+            var someValue = await getAPIKey()
+            var apikey = someValue.result.auth.apiKey
+
+            const getDoorID = () => new Promise((resolve, reject) => {
+                var options = {url:'https://tti.tiwiconnect.com/api/devices/' + doorid + '',method:'GET',json:JSON.parse('{"username":"' + queryData.email + '","password":"' + cleanpass + '"}')}
+                request(options, (err, res, body) => {
+                    if (err) return reject(err)
+                        resolve(body)
+                })
+            })
+
+            const getDoorIDController = async function() {
+                var someValue = await getDoorID()
+                //console.log(someValue.result[0].varName)
+                var doorid = someValue.result[0].varName
+
+                if (cmdtype == 0) {
+                    var ws = new WebSocket('wss://tti.tiwiconnect.com/api/wsrpc', 'echo-protocol');
+                    ws.onopen = function()
+                    {
+                        ws.send(JSON.parse(JSON.stringify('{"jsonrpc":"2.0","id":3,"method":"srvWebSocketAuth","params": {"varName": "' + queryData.email + '","apiKey": "' + apikey + '"}}')));
+                        function freeze(time) {
+                            const stop = new Date().getTime() + time;
+                            while(new Date().getTime() < stop);
+                        }
+                        freeze(250);
+                        ws.send(JSON.parse(JSON.stringify('{"jsonrpc":"2.0","method":"gdoModuleCommand","params":{"msgType":16,"moduleType":5,"portId":7,"moduleMsg":{"' + cmd + '":' + cmdstate + '},"topic":"' + doorid +'"}}')));
+                        response.end("Ran Command");
+                        ws.close()
+                    }
+                } else if (cmdtype == 1) {
+                    var request = require('request');
+
+                    const getStatus = () => new Promise((resolve, reject) => {
+                        console.log('-->Getting Status')
+                        var options = {url:'https://tti.tiwiconnect.com/api/devices/' + doorid + '',method:'GET',json:JSON.parse('{"username":"' + queryData.email + '","password":"' + cleanpass + '"}')}
+                            function freeze(time) {
+                                const stop = new Date().getTime() + time;
+                                while(new Date().getTime() < stop);
+                            }
+                            freeze(3000);
+
+                            request(options, (err, res, body) => {
+                            if (err) return reject(err)
+                            resolve(body)
+                        })
+                    })
+
+                    const getStatusController = async function() {
+                        console.log('->Asking for status')
+                        var statusValue = await getStatus()
+                        console.log('->Got status')
+                        for(var device in statusValue.result[0].deviceTypeMap) {
+                            if (device.includes('garageDoor')) {
+                                var doorval = statusValue.result[0].deviceTypeMap[device].at.doorState.value
+                                console.log('garageDoor --> ' + doorval)
+                            }
+                            else if (device.includes('garageLight')) {
+                                var lightval = statusValue.result[0].deviceTypeMap[device].at.lightState.value
+                                console.log('garageLight --> ' + lightval)
+                            }
+                            else if (device.includes('backupCharger')) {
+                                var batval = statusValue.result[0].deviceTypeMap[device].at.chargeLevel.value
+                                console.log('backupCharger --> ' + batval)
+                            }
+                            if (batval == null) {
+                                    var batval = 'NA'
+                            }
+                        }
+                        response.end('status:' + String(lightval) + ':' + String(doorval) + ':' + String(batval))
+
+                        console.log('--------')
+                        console.log('TYPE MAP')
+                        console.log('--------')
+                        console.log(statusValue.result[0].deviceTypeMap)
+                        console.log('--------')
+                        console.log('METADATA')
+                        console.log('--------')
+                        console.log(statusValue.result[0].varName)
+                        console.log('--------')
+                        console.log('--------')
+                        //console.log('Door: ' + statusValue.result[0].metadata.name)
+                        console.log('Door ID: ' + statusValue.result[0].varName)
+                        console.log('Door Val: ' + doorval)
+                        console.log('Light Val: ' + lightval)
+                        console.log('Battery Val: ' + batval)
+                        console.log('--------')
+                        console.log('--------')
+                    }
+                    getStatusController()
+
+                }
+            }
+
+            getDoorIDController()
+        }
+    getAPIKeyController()
+
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+}
+)
---
+++ devicetypes/madj42/ryobi-garage-door.src/ryobi-garage-door.groovy
@@ -0,0 +1,234 @@
+/*
+* Author: Justin Dybedahl
+* Ryobi GDO200/GDO201/GDO500 Device Handler
+* v2.7
+* Thanks to @Projectskydroid for the modifications.
+* 2020-03-23 updated by crackyn to handle multiple GDO
+*/
+
+def clientVersion() {
+    return "2.7"
+}
+
+preferences {
+	section("Configuration Parameters"){
+		input "email", "email", title: "Email Address",required: true
+		input "pass","password", title: "Password",required:true
+		input "internal_ip", "text", title: "Internal IP", required: true
+		input "internal_port", "text", title: "Internal Port (default is 3042)", required: true
+		input title: "", description: "Ryobi GDO200 Device Handler v${clientVersion()}", displayDuringSetup: false, type: "paragraph", element: "paragraph", required: true
+                input title: "", description: "http://www.github.com/Madj42/RyobiGDO", displayDuringSetup: false, type: "paragraph", element: "paragraph"
+		input "pollInterval", "number", title: "Polling Interval", description: "Change polling frequency (in minutes)", defaultValue:4, range: "1..59", required: true, displayDuringSetup: true
+		input "door_id", "text", title: "Door ID",required: true
+    }
+}
+
+
+
+
+metadata {
+	definition (name: "Ryobi Garage Door", namespace: "madj42", author: "Justin Dybedahl") {
+		capability "Actuator"
+		capability "Door Control"
+		capability "Garage Door Control"
+		capability "Switch"
+		capability "Sensor"
+		capability "Polling"
+		capability "Momentary"
+		capability "Relay Switch"
+		capability "Refresh"
+		capability "Battery"
+	}
+
+    attribute "switch", "string"
+
+    command "on"
+    command "off"
+    command "open"
+    command "close"
+
+	// simulator metadata
+	simulator {
+	}
+
+		// UI tile definitions
+	tiles {
+  	    multiAttributeTile(name: "door", type: "lighting", width: 6, height: 4, canChangeIcon: false) {
+			tileAttribute("device.door", key: "PRIMARY_CONTROL") {
+            attributeState "closed", label: 'Door Closed', action: "door control.open", icon: "st.Home.home2", backgroundColor: "#ffffff"
+			attributeState "open", label: 'Door Open', action: "door control.close", icon: "st.Home.home2", backgroundColor: "#79b821"
+            attributeState "closing", label:'Door Closing', action:"door control.close", icon:"st.Home.home2", backgroundColor:"#00a0dc"
+			attributeState "opening", label:'Door Opening', action:"door control.open", icon:"st.Home.home2", backgroundColor:"#79b821"
+            }
+        }
+        standardTile("button2", "device.switch", width: 2, height: 2, canChangeIcon: false) {
+			state "off", label: 'Light Off', action: "switch.on", icon: "st.Lighting.light11", backgroundColor: "#ffffff"
+			state "on", label: 'Light On', action: "switch.off", icon: "st.Lighting.light11", backgroundColor: "#79b821"
+		}
+        valueTile("icon", "device.icon", inactiveLabel: false, decoration: "flat", width: 2, height: 1) {
+            state "default", label: '', icon: "https://logo-png.com/logo/ryobi-logo.png"
+        }
+        valueTile("battery", "device.Battery", inactiveLabel: false, decoration: "flat", width: 2, height: 1) {
+            state "Battery", label: 'Battery: ${currentValue}%'
+        }
+        standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat") {
+            state "default", action:"refresh", icon:"st.secondary.refresh"
+        }
+        valueTile("icon", "device.icon", inactiveLabel: false, decoration: "flat", width: 2, height: 1) {
+            state "default", label: '', icon: "https://logo-png.com/logo/ryobi-logo.png"
+		}
+		standardTile("open", "device.door", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'open', action:"door control.open", icon:"st.Home.home2"
+        }
+		standardTile("close", "device.door", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'close', action:"door control.close", icon:"st.Home.home2"
+        }
+		main "door"
+			details (["door","button","button2","refresh","battery","icon","open","close"])
+            }
+}
+
+def poll() {
+getStatus()
+}
+
+def refresh() {
+poll()
+}
+
+def updated() {
+    	runIn(3, "updateDeviceNetworkID")
+		schedule("0 0/${settings.pollInterval} * * * ?", poll)
+}
+
+def parse(String description){
+//log.debug "Parse called"
+	def msg = parseLanMessage(description)
+    if (msg.body.startsWith("status:")) {
+    	def batstatus = msg.body.split(':')[3]
+    	def doorstatus = msg.body.split(':')[2]
+    	def lightstatus = msg.body.split(':')[1]
+	if (batstatus == "255") {
+		sendEvent(name: "Battery", value: 0)
+	} else if (batstatus == null) {
+		sendEvent(name: "Battery", value: 0)
+	} else if (batstatus == 'NA') {
+		sendEvent(name: "Battery", value: 0)
+	} else {
+	sendEvent(name: "Battery", value: batstatus)
+	}
+    	if (lightstatus == "false") {
+        //log.debug "Light OFF"
+        sendEvent(name: "switch", value: "off")
+   		} else if (lightstatus == "true") {
+        //log.debug "Light ON"
+        sendEvent(name: "switch", value: "on")
+        }
+       	if (doorstatus == "0") {
+        //log.debug "Door Closed"
+        sendEvent(name: "door", value: "closed")
+   		} else if (doorstatus == "1") {
+        //log.debug "Door Open"
+        sendEvent(name: "door", value: "open")
+        } else if (doorstatus == "2") {
+        //log.debug "Door Closing"
+        sendEvent(name: "door", value: "closing")
+        } else if (doorstatus == "3") {
+        //log.debug "Door Opening"
+        sendEvent(name: "door", value: "opening")
+        }
+    }
+}
+
+def on() {
+def result = new physicalgraph.device.HubAction(
+				method: "GET",
+				path: "/?name=lighton&doorid=${door_id}&apikey=${apikey}&email=${email}&pass=${pass}",
+				headers: [
+				HOST: "${internal_ip}:${internal_port}"
+				]
+				)
+
+			sendHubCommand(result)
+			sendEvent(name: "switch", value: "on")
+            runIn(5,getStatus)
+			//log.debug "Turning light ON"
+            }
+
+def off() {
+def result = new physicalgraph.device.HubAction(
+				method: "GET",
+				path: "/?name=lightoff&doorid=${door_id}&apikey=${apikey}&email=${email}&pass=${pass}",
+				headers: [
+				HOST: "${internal_ip}:${internal_port}"
+				]
+				)
+
+			sendHubCommand(result)
+			sendEvent(name: "switch", value: "off")
+           runIn(5,getStatus)
+			//log.debug "Turning light OFF"
+	}
+
+def open() {
+def result = new physicalgraph.device.HubAction(
+				method: "GET",
+				path: "/?name=dooropen&doorid=${door_id}&apikey=${apikey}&email=${email}&pass=${pass}",
+				headers: [
+				HOST: "${internal_ip}:${internal_port}"
+				]
+				)
+
+			sendHubCommand(result)
+			sendEvent(name: "door", value: "opening")
+            runIn(5,getStatus)
+            runIn(17,getStatus)
+			//log.debug "OPENING Garage Door"
+            }
+
+def close() {
+def result = new physicalgraph.device.HubAction(
+				method: "GET",
+				path: "/?name=doorclose&doorid=${door_id}&apikey=${apikey}&email=${email}&pass=${pass}",
+				headers: [
+				HOST: "${internal_ip}:${internal_port}"
+				]
+				)
+
+			sendHubCommand(result)
+			sendEvent(name: "door", value: "closing")
+            runIn(5,getStatus)
+            runIn(21,getStatus)
+			//log.debug "CLOSING Garage Door"
+            }
+
+def getStatus() {
+	def result = new physicalgraph.device.HubAction(
+				method: "GET",
+				path: "/?name=status&doorid=${door_id}&apikey=${apikey}&email=${email}&pass=${pass}",
+				headers: [
+				HOST: "${internal_ip}:${internal_port}"
+				])
+			sendHubCommand(result)
+			//log.debug "Getting Status"
+	}
+
+private String convertIPtoHex(ipAddress) {
+    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
+    //log.debug "IP address entered is $ipAddress and the converted hex code is $hex"
+    return hex
+
+}
+
+private String convertPortToHex(port) {
+    String hexport = port.toString().format( '%04x', port.toInteger() )
+    //log.debug hexport
+    return hexport
+}
+
+def updateDeviceNetworkID() {
+	//log.debug "Executing 'updateDeviceNetworkID'"
+    def iphex = convertIPtoHex(internal_ip).toUpperCase()
+    def porthex = convertPortToHex(internal_port).toUpperCase()
+	device.setDeviceNetworkId(iphex + ":" + porthex)
+}
